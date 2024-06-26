95a13e125abfb2e93be76cd08c74e496cd4e91cf
seyed ali <108273686+seyed-ali-khorasani@users.noreply.github.com>
2024-06-26 19:55:54 +0330

    create y and print the created shares

--- secret_sharing.py
+++ secret_sharing.py
@@ -10,8 +10,21 @@ def check_prime(mod):
 
 def create_share(secret, n, t, MOD):
     zarib = [random.randint(0, MOD) for _ in range(t-1)]
+    zarib.insert(0, secret)  # The first coefficient is the secret
+    shares = [(i, calculate_y(zarib, i, MOD, secret)) for i in range(1, n + 1)]
     return shares
 
+
+def calculate_y(zarib, x, MOD, secret):
+    result = 0
+    power = 0
+    for zari in zarib:
+        now = 1
+        for i in range(power):
+            now= now * x
+        result = result + (zari * now) 
+        power = power + 1
+    return result % MOD
 
 
 
@@ -37,4 +50,6 @@ elif not mod_is_prime:
 if t_is_lessthan and mod_is_prime:
     shares = create_share(secret, n, t, MOD)
 
-    
+    print("people shares: ")
+    for share in shares:
+        print(f"x: {share[0]}, y: {share[1]}")
