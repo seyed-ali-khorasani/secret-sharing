93b3dff57ac17bea8db89d391e56a2c63422ec20
seyed ali <108273686+seyed-ali-khorasani@users.noreply.github.com>
2024-06-26 19:34:05 +0330

    create coefficients of equation

--- secret_sharing.py
+++ secret_sharing.py
@@ -1,9 +1,16 @@
+import random
+
 
 
 def check_prime(mod):
     for i in range(2,mod):
         if mod % i == 0: return False
     return True
+
+
+def create_share(secret, n, t, MOD):
+    zarib = [random.randint(0, MOD) for _ in range(t-1)]
+    return shares
 
 
 
@@ -14,11 +21,20 @@ def check_prime(mod):
 t_is_lessthan = True
 mod_is_prime = True
 n = int(input("people number: "))
+t = int(input("threshold: "))
 MOD = int(input("MOD: "))
 secret = int(input("secret: "))
-t = int(input("threshold: "))
+
 
 mod_is_prime = check_prime(MOD)
 if t > n:
-    print("Threshold t cannot be greater than the number of people n.")
+    print("Threshold must be greater than number of people.")
     t_is_lessthan = False
+elif not mod_is_prime:
+    print("your mod must be prime")
+
+
+if t_is_lessthan and mod_is_prime:
+    shares = create_share(secret, n, t, MOD)
+
+    
